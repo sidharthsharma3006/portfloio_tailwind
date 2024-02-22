e3d12c724528d5dc9e69ae8839e3c10944bccc3f
Saksham Sharma <sakshamsharma2025@gmail.com>
2024-02-22 11:09:56 +0530

    first commit

---
+++ src/components/About.jsx
@@ -0,0 +1,17 @@
+import aboutSvg from '../assets/about.svg'; 
+import SectionTitle from './SectionTitle'; 
+
+const About = ()=>{
+    return <section className='bg-white py-20'>
+        <div className='align-element grid md:grid-cols-2 items-center gap-16'>
+            <img src={aboutSvg} className='w-full h-64' /> 
+            <article>
+                <SectionTitle text='code and coffee'/> 
+                <p className='text-slate-600 mt-8 leading-loose'>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque voluptatem illo, vel aliquam cum eligendi provident? Et, repudiandae adipisci? Quibusdam explicabo libero in! Expedita odit ab aliquam, facilis sequi non?
+                </p>
+            </article>
+        </div>
+    </section>; 
+}; 
+export default About; 
---
+++ src/components/Projects.jsx
@@ -0,0 +1,19 @@
+import SectionTitle from "./SectionTitle"; 
+import ProjectCard from "./ProjectCard"; 
+import { projects } from "../data"; 
+
+const Projects = ()=>{
+    return(
+       <section className="py-20 align-element" id="projects">
+         <SectionTitle text='Web Creations'/> 
+          <div className="py-16 grid lg:grid-cols-2 xl:grid-cols-3 gap-8"> 
+           {
+            projects.map((project)=>{
+              return <ProjectCard key={project.id} {...project}/>
+            })
+           }
+          </div>
+       </section>
+    )
+} 
+export default Projects; 
